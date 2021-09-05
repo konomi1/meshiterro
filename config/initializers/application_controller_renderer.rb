15c53c6978c30f4d62bb08a672361381befc3a01
konomi1 <donnguri555@gmail.com>
2021-09-05 08:38:39 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e55c1cf7d46cd8393c057ea0873f498a1627d11257c2ee9e121fe3836973005945018a65dd8716e5ef0937ad707f8c56e2245290381bea93e0af97de2db5cc6f'
